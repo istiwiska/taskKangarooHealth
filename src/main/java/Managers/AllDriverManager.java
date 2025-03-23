87297691f4c3db83b97ecf6533ecbe461f67a259
USER <istiwiska@abs.global>
2025-03-23 21:01:55 +0700

    Edit selenium-tests.yml

--- src/main/java/Managers/AllDriverManager.java
+++ src/main/java/Managers/AllDriverManager.java
@@ -40,7 +40,7 @@ public class AllDriverManager {
 //                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Driver/chromedriver134.exe");
 //                System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless=new");
+                options.addArguments("--user-data-dir=/tmp/chrome-profile-" + System.currentTimeMillis());
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("start-maximized");
